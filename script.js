54b9d9838d7791ad192a75abce833547aec03aad
Mateus Lira <mateus.lrg@gmail.com>
2025-08-25 07:02:28 -0300

    script

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function click(){
 
 function iniciarJogo(){
     click();
-    musica();
+    //musica();
     document.getElementById("tela-inicio").style.display="none";
     document.getElementById("tela-jogo").style.display="flex";
 }
@@ -41,43 +41,101 @@ function jogar(){
   verificar();
 }
 
+// verificação teste-----------------------------------------
+// function verificarFileiraCaramelo(){
+//   let slot1 = document.getElementById("tile1").src;
+//   let slot2 = document.getElementById("tile2").src;
+//   let slot3 = document.getElementById("tile3").src;
+//   let carta = "tile1.png";
+
+//   for(let i=0; i<2000; i++){
+//    if( slot1.endsWith(carta) && slot2.endsWith(carta) && slot3.endsWith(carta) ){
+//     console.log("GANHOU")
+//     return true;
+//     }
+//      console.log("Teste B 0"+i);
+//   }
+//     return false;
+// }
+// -----------------------------------------
+
+
+// function verificarFileira1(){
+//   let carta = "tile1.png";
+
+//   for(let i=0; i<4000; i++){
+//     // sorteia novamente os tiles da fileira 1
+//     for (let j=1; j<=3; j++){
+//       let id = Math.floor(Math.random()*5)+1;
+//       document.getElementById("tile"+j).src = "./img/tile"+id+".png";
+//     }
+
+//     // lê os src atualizados
+//     let slot1 = document.getElementById("tile1").src;
+//     let slot2 = document.getElementById("tile2").src;
+//     let slot3 = document.getElementById("tile3").src;
+
+//     if(slot1.endsWith(carta) && slot2.endsWith(carta) && slot3.endsWith(carta)){
+//       console.log("GANHOU na tentativa " + i);
+//       return true;
+//     }
+//   }
+
+//   console.log("Não conseguiu ganhar");
+//   return false;
+// }
+
+
+
 function verificarFileira1(){
   let slot1 = document.getElementById("tile1").src;
-  let slot2 = document.getElementById("tile4").src;
-  let slot3 = document.getElementById("tile7").src;
+  let slot2 = document.getElementById("tile2").src;
+  let slot3 = document.getElementById("tile3").src;
   let carta = "tile1.png";
 
-   if( slot1.endsWith(carta) && slot2.endsWith(carta) && slot3.endsWith(carta) ){
-    return true;
-  }else{
-    return false;
+  for(let i=0; i<10; i++){
+    if( slot1.endsWith(carta) && slot2.endsWith(carta) && slot3.endsWith(carta) ){
+      console.log("GANHOU")
+      return true;
+    }
+     console.log("Teste A 0"+i);
   }
+    return false;
+
 }
 
 function verificarFileira2(){
-  let slot4 = document.getElementById("tile2").src;
+  let slot4 = document.getElementById("tile4").src;
   let slot5 = document.getElementById("tile5").src;
-  let slot6 = document.getElementById("tile8").src;
+  let slot6 = document.getElementById("tile6").src;
   let carta = "tile1.png";
 
+  for(let i=0; i<10; i++){
    if( slot4.endsWith(carta) && slot5.endsWith(carta) && slot6.endsWith(carta) ){
+    console.log("GANHOU")
     return true;
-  }else{
-    return false;
+    }
+     console.log("Teste B 0"+i);
   }
+    return false;
 }
+
 function verificarFileira3(){
-  let slot7 = document.getElementById("tile3").src;
-  let slot8 = document.getElementById("tile6").src;
+  let slot7 = document.getElementById("tile7").src;
+  let slot8 = document.getElementById("tile8").src;
   let slot9 = document.getElementById("tile9").src;
   let carta = "tile1.png";
 
+  for(let i=0; i<10; i++){
    if( slot7.endsWith(carta) && slot8.endsWith(carta) && slot9.endsWith(carta) ){
+    console.log("GANHOU")
     return true;
-  }else{
-    return false;
+    }
+    console.log("Teste C 0"+i);
   }
+    return false;
 }
+
 function verificar(){
   // let fileira1 = [slot1, slot2, slot3];
   // let fileira2 = [slot4, slot5, slot6];
@@ -89,3 +147,40 @@ function verificar(){
     alert("Perdeu");
   }
 }
+
+// --------------------- Script do formulario ----------------------
+
+let saldo = 0;
+
+  function atualizarSaldo() {
+    document.getElementById("saldo").innerText =
+      "Saldo: R$ " + saldo.toFixed(2).replace(".", ",");
+  }
+
+  function depositar() {
+    let valor = parseFloat(document.getElementById("deposito").value);
+    if (isNaN(valor) || valor <= 0) {
+      alert("Por favor, insira um valor válido para depositar.");
+      return;
+    }
+    saldo += valor;
+    atualizarSaldo();
+    document.getElementById("deposito").value = "";
+  }
+
+  function sacar() {
+    let valor = parseFloat(document.getElementById("saque").value);
+    if (isNaN(valor) || valor <= 0) {
+      alert("Informe um valor válido para saque!");
+      return;
+    }
+    if (valor > saldo) {
+      alert("Saldo insuficiente!");
+      return;
+    }
+    saldo -= valor;
+    atualizarSaldo();
+    document.getElementById("saque").value = "";
+  }
+
+  atualizarSaldo();
